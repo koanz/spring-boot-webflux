02e7982ee5153e109e59904a03f2343127905a33
Cristian <cristiananzawa@gmail.com>
2025-02-20 11:27:11 -0300

    categories created

--- src/main/java/com/ideas/springboot/webflux/app/SpringBootWebfluxApplication.java
+++ src/main/java/com/ideas/springboot/webflux/app/SpringBootWebfluxApplication.java
@@ -1,5 +1,7 @@
 package com.ideas.springboot.webflux.app;
 
+import com.ideas.springboot.webflux.app.documents.Category;
+import com.ideas.springboot.webflux.app.repositories.CategoryRepository;
 import com.ideas.springboot.webflux.app.repositories.ProductRepository;
 import com.ideas.springboot.webflux.app.documents.Product;
 import org.slf4j.Logger;
@@ -10,6 +12,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.Date;
 
@@ -17,7 +20,10 @@ import java.util.Date;
 public class SpringBootWebfluxApplication implements CommandLineRunner {
 
 	@Autowired
-	private ProductRepository dao;
+	private ProductRepository productRepository;
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Autowired
 	private ReactiveMongoTemplate mongoTemplate;
@@ -31,21 +37,32 @@ public class SpringBootWebfluxApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		mongoTemplate.dropCollection("products").subscribe();
+		mongoTemplate.dropCollection("categories").subscribe();
 
-		Flux.just(
-				new Product("Notebook", 255.99),
-				new Product("Backpack", 85.50),
-				new Product("Mouse Logitech Superlight", 120.0),
-				new Product("HyperX Cloudflight", 145.0),
-				new Product("JBL Charge 4", 132.0),
-				new Product("Sony Vaio i3 1.4 8gb 500gb SSD", 285.0),
-				new Product("Samsung S23 FE 8gb 128GB", 515.0),
-				new Product("Keyboard Logitech p200", 38.0),
-				new Product("Smart TV Samsung s7000 50", 432.0)
-		).flatMap(product -> {
-			product.setCreatedAt(new Date());
-			return dao.save(product);
-				})
+		Category techonology = new Category("Technology");
+		Category travel = new Category("Travel");
+		Category accesories = new Category("Accesories");
+
+		Flux.just(techonology, travel, accesories)
+				.flatMap(categoryRepository::save)
+				.doOnNext(c -> logger.info("Category created: " + c.getName()))
+				.thenMany(
+						Flux.just(
+								new Product("Notebook", 255.99, techonology),
+								new Product("Backpack", 85.50, travel),
+								new Product("Mouse Logitech Superlight", 120.0, techonology),
+								new Product("HyperX Cloudflight", 145.0, techonology),
+								new Product("JBL Charge 4", 132.0, techonology),
+								new Product("Sony Vaio i3 1.4 8gb 500gb SSD", 285.0, techonology),
+								new Product("Samsung S23 FE 8gb 128GB", 515.0, techonology),
+								new Product("Keyboard Logitech p200", 38.0, techonology),
+								new Product("Smart TV Samsung s7000 50", 432.0, techonology),
+								new Product("Keychain Independent 50 Anniversary", 18.99, accesories)
+						).flatMap(product -> {
+							product.setCreatedAt(new Date());
+							return productRepository.save(product);
+						})
+				)
 		.subscribe(product -> logger.info("Insert of Producto: " + product.getId()));
 	}
 }
