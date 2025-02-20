148fe636f13494c686b4448c06798d5dc3f29c06
Cristian <cristiananzawa@gmail.com>
2025-02-20 11:29:46 -0300

    category attribute added

--- src/main/java/com/ideas/springboot/webflux/app/documents/Product.java
+++ src/main/java/com/ideas/springboot/webflux/app/documents/Product.java
@@ -1,5 +1,9 @@
 package com.ideas.springboot.webflux.app.documents;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -10,8 +14,16 @@ import java.util.Date;
 public class Product {
     @Id
     private String id;
+
+    @NotEmpty(message = "Field name cannot be empty.")
     private String name;
+
+    @NotNull(message = "Field price cannot be empty.")
+    @Positive(message = "Field price must be greater than zero (0).")
     private Double price;
+
+    @Valid
+    private Category category;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date createdAt;
@@ -19,6 +31,11 @@ public class Product {
     public Product(String name, Double price) {
         this.name = name;
         this.price = price;
+    }
+
+    public Product(String name, Double price, Category category) {
+        this(name, price);
+        this.category = category;
     }
 
     public Product() {
@@ -46,6 +63,14 @@ public class Product {
 
     public void setPrice(Double price) {
         this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
     }
 
     public Date getCreatedAt() {
